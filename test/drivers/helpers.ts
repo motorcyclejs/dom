18ab2ea114c7319ad995bba631e6dcd815362cdd
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-11-11 19:52:57 -0500

    fix(text): fix incorrect tests

--- test/drivers/helpers.ts
+++ test/drivers/helpers.ts
@@ -1,18 +1,20 @@
 import * as assert from 'assert';
 import { findElementBySelector } from '../../src/drivers/helpers';
 
+let i = 0;
+
 describe('DOM Helpers', () => {
   describe('findElementBySelector', () => {
     it('should throw error if it cannot find an element', () => {
-      const uniqueSelector = 'UniqueSelector' + Math.random();
+      const uniqueSelector = 'UniqueSelector' + ++i;
 
       assert.throws(() => {
         findElementBySelector(uniqueSelector);
       });
     });
 
-    it('should reeturn an element it can find', () => {
-      const uniqueSelector = 'UniqueSelector' + Math.random();
+    it('should return an element it can find', () => {
+      const uniqueSelector = 'UniqueSelector' + ++i;
       const element = document.createElement('div');
       element.id = uniqueSelector;
       document.body.appendChild(element);

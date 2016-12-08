e5eb613bf535f9183e8ac9340b1efe5b6348a2e2
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-12-08 11:24:57 -0500

    feat(IsolateModule): add data-isolate to DOM elements

--- src/modules/IsolateModule.ts
+++ src/modules/IsolateModule.ts
@@ -1,27 +1,6 @@
 import { Module, VNode } from '../types';
 
 export class IsolateModule implements Module {
-  constructor(
-    private _isolatedElements: Map<string, HTMLElement> = new Map(),
-  ) { }
-
-  public findElement(scope: string): HTMLElement | null {
-    return this._isolatedElements.get(scope) || null;
-  }
-
-  public findScope(element: HTMLElement): string | null {
-    const entries = Array.from(this._isolatedElements.entries());
-
-    for (let i = 0; i < entries.length; ++i) {
-      const [scope, isolatedElement] = entries[i];
-
-      if (isolatedElement === element)
-        return scope;
-    }
-
-    return null;
-  }
-
   public create(formerVNode: VNode, vNode: VNode) {
     this.setAndRemoveScopes(formerVNode, vNode);
   }
@@ -30,35 +9,30 @@ export class IsolateModule implements Module {
     this.setAndRemoveScopes(formerVNode, vNode);
   }
 
-  public remove(vNode: VNode, remove: Function) {
-    this.removeScope(vNode, remove);
-  }
-
-  public destroy(vNode: VNode) {
-    this.removeScope(vNode);
-  }
-
   private setAndRemoveScopes(formerVNode: VNode, vNode: VNode) {
-    const formerScope = scopeFromVNode(formerVNode);
-    const scope = scopeFromVNode(vNode);
-
-    if (!scope && !formerScope) return;
-
-    if (formerScope)
-      this._isolatedElements.delete(formerScope);
-
-    if (scope)
-      this._isolatedElements.set(scope, vNode.elm);
-  }
-
-  private removeScope(vNode: VNode, remove: Function = noop) {
     const scope = scopeFromVNode(vNode);
 
     if (!scope) return;
 
-    this._isolatedElements.delete(scope);
+    (vNode.elm as HTMLElement).setAttribute('data-isolate', scope);
 
-    remove();
+    addScopeToChildren(vNode.elm.children, scope);
+  }
+}
+function addScopeToChildren(children: HTMLCollection, scope: string) {
+  if (!children) return;
+
+  const count = children.length;
+
+  for (let i = 0; i < count; ++i) {
+    const child = children[i];
+
+    if (child.hasAttribute('data-isolate')) continue;
+
+    child.setAttribute('data-isolate', scope);
+
+    if (child.children)
+      addScopeToChildren(child.children, scope);
   }
 }
 

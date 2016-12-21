d93d9ea7bbabf6430a83a0b087ea603e46174180
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-12-21 15:19:24 -0500

    chore(dom): update build and test systems

---
+++ .config/karma.conf.js
@@ -0,0 +1,52 @@
+module.exports = function (config) {
+  const options =
+    {
+      basePath: '../',
+
+      files: [
+        'src/**/*.ts',
+        'test/**/*.ts',
+      ],
+
+      frameworks: [
+        'mocha',
+        'karma-typescript',
+      ],
+
+      preprocessors: {
+        '**/*.ts': ['karma-typescript'],
+      },
+
+      reporters: ['progress', 'karma-typescript'],
+
+      customLaunchers: {
+        Chrome_travis_ci: {
+          base: 'Chrome',
+          flags: ['--no-sandbox']
+        }
+      },
+
+      browsers: [],
+
+      karmaTypescriptConfig: {
+        tsconfig: 'tsconfig.json',
+        reports: {
+          "html": "coverage",
+          "lcovonly": "coverage",
+        }
+      }
+    }
+
+  if (process.env.UNIT)
+    options.browsers.push('Chrome')
+
+  if (process.env.TRAVIS) {
+    options.browsers.push('Chrome_travis_ci', 'Firefox')
+    options.reporters.push('coveralls')
+  }
+
+  if (options.browsers.length === 0)
+    options.browsers.push('Chrome', 'Firefox')
+
+  config.set(options);
+}
